6b641bd87090db6ce123c87c2b1b407d7b7b81d2
Bui Manh <buimanh17112001@gmail.com>
2021-06-02 00:18:18 +0700

    manh

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-axios.defaults.baseURL = "https://webmanagement-api.herokuapp.com/api"
+axios.defaults.baseURL = "https://icomic-api.herokuapp.com/api"
 // axios.defaults.baseURL = "http://localhost:3999/api"
 
 const request = (endpoint, method, data) => {
